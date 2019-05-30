c924196a72e9e88d5342ca04ae93aebf345518fc
Yan C Lin <yonalin1@gmail.com>
2019-05-30 20:05:42 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,8 +1,7 @@
-export default function manageTodo(state = {
-  todos: [],
-}, action) {
-
-  console.log("reducer received this action:", action);
-
-  return state;
-}
+export default function manageTodo(
+  state = {
+    todos: [
+      { text: 'buy groceries' },
+      { text: 'watch netflix' },
+    ]
+  }
