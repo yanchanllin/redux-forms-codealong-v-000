f62c96264109bc174d30b285ca08618c8f92ea44
Yan C Lin <yonalin1@gmail.com>
2019-05-30 20:11:53 +0000

    Automatically backed up by Learn

--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -4,13 +4,11 @@ export default function manageTodo(
   },action) {
     switch (action.type) {
       case 'ADD_TODO':
-        
+
       console.log({ todos: state.todos.concat(action.payload.text) });
 
      return { todos: state.todos.concat(action.payload.text) };
       default:
-        return state; 
+        return state;
     }
   }
-    
-
